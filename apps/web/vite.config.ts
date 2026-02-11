423564c96d7552a1d81279bffbb44393d065e994
BONHEUR-EDISON <133498192+BONHEUR-EDISON@users.noreply.github.com>
2026-02-11 17:56:21 +0100

    Update vite.config.ts

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -3,12 +3,13 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
-  root: path.resolve(__dirname), // ou "./" si tu es déjà dans apps/web
+   root: path.resolve(__dirname, "apps/web"),
   plugins: [react()],
-  build: {
-    outDir: "dist", // Vite va générer apps/web/dist
-    emptyOutDir: true
-  },
+build: {
+  outDir: "dist", // Vercel détectera apps/web/dist
+  emptyOutDir: true
+},
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src")
