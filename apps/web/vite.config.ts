f597ae3c562af694dbb3dac39d25c3cd7441ad08
BONHEUR-EDISON <bonheurluundoedison@gmail.com>
2026-02-11 16:48:33 +0100

    msg+6

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,6 +1,12 @@
+// vite.config.ts
 import { defineConfig } from "vite";
-import react from '@vitejs/plugin-react'
-// https://vite.dev/config/
+import react from "@vitejs/plugin-react";
+
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: "dist", // <-- c’est ici que Vite va générer le dossier
+    emptyOutDir: true
+  },
+  root: "./" // assure-toi que c’est bien apps/web si config spécifique
+});
